ae00127fba7deae89efb18b29f7908bac342225a
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-11-14 14:18:08 -0500

    -

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const dbPromise = initdb();
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+  await tx.done;
+};
 
-initdb();
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  await tx.done;
+  return allContent;
+};
